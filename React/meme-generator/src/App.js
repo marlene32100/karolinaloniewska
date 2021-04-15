adfc4765eb01653805e3927303777a6484b4e7e8
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-04-15 09:27:09 +0200

    Initial commit

---
+++ React/meme-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+import MemeGenerator from "./MemeGenerator.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/meme-generator/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <img
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+          alt="Meme troll image"
+        />
+        <p>Meme Generator</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
